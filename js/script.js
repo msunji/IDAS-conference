94fb8b5b7eefb8f13921bf8c0480ad9f43448232
msunji <mae.sunji@gmail.com>
2018-03-20 20:21:43 +0800

    Added smooth scrolling

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@ const navLinks = document.querySelectorAll('.nav-link');
 function smoothScroll(e) {
 	e.preventDefault();
 	var element = this.getAttribute("href");
-	console.log(${element});
-	document.querySelector(${element}).scrollIntoView({ behavior: 'smooth' });
+	document.querySelector(`${element}`).scrollIntoView({ behavior: 'smooth' });
 }
 
 navLinks.forEach(link => link.addEventListener('click', smoothScroll));
