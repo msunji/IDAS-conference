5f0ef71664a5e46dd067be29d7e76776d8c28207
msunji <mae.sunji@gmail.com>
2018-03-21 20:37:48 +0800

    Fixed dropdown again

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-const desktopNav = document.querySelector('.desktop-nav');
 const navLinks = document.querySelectorAll('.nav-link');
 const ctaLinks = document.querySelectorAll('.cta-button-nav');
-const dropdownLink = document.querySelector('.dropdown-link');
+const dropdownDesktop= document.querySelector('.dropdown-link');
 const dropDown = document.getElementById('dropdown-desktop');
-const downCarat = document.querySelector('.down-carat');
 
 // Smooth Scroll
 function smoothScroll(e) {
@@ -13,20 +11,33 @@ function smoothScroll(e) {
 }
 
 // Dropdown Menu
-function dropdownHandler(e) {
+function dropdownHandlerDesktop(e) {
 	e.stopPropagation();
+	const downCarat = document.querySelector('.down-carat');
 	dropDown.classList.toggle('show');
 	downCarat.classList.toggle('active');
 }
+
 // Hide dropdown if you click on something else
 function hideMenuHandler(e) {
 	dropDown.classList.remove('show');
 }
 
 // Sticky navbar
+// function stickyNavHandler(e) {
+// 	const navbar = document.querySelector('.main-nav');
+// 	const sticky = navbar.offsetTop;
+// 	if(window.pageYOffset >= sticky) {
+// 		navbar.classList.add("sticky-nav")
+// 	}
+// 	else {
+// 		navbar.classList.remove("sticky-nav");
+// 	}
+// }
 
 // Event Listeners
 navLinks.forEach(link => link.addEventListener('click', smoothScroll));
 ctaLinks.forEach(link => link.addEventListener('click', smoothScroll));
-dropdownLink.addEventListener('click', dropdownHandler);
+dropdownDesktop.addEventListener('click', dropdownHandlerDesktop);
 window.addEventListener('click', hideMenuHandler);
+// window.addEventListener('scroll', stickyNavHandler);
