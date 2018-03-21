48381ae5faa91e0a37c5a7022b0978091d852c3f
msunji <mae.sunji@gmail.com>
2018-03-21 17:07:18 +0800

    Fixed dropdown

--- js/script.js
+++ js/script.js
@@ -1,42 +1,29 @@
 const desktopNav = document.querySelector('.desktop-nav');
 const navLinks = document.querySelectorAll('.nav-link');
 const ctaLinks = document.querySelectorAll('.cta-button-nav');
-// const dropdownLink = document.querySelector('.dropdown-link');
-const dropDown = document.querySelector('.dropdown-menu');
+const dropdownLink = document.querySelector('.dropdown-link');
+const dropDown = document.getElementById('dropdown-desktop');
 const downCarat = document.querySelector('.down-carat');
 
 // Smooth Scroll
 function smoothScroll(e) {
 	e.preventDefault();
 	var element = this.getAttribute("href");
-	console.log(element);
 	document.querySelector(element).scrollIntoView({ behavior: 'smooth' });
 }
 
 // Dropdown Menu
-function dropdownMenu(e) {
-	dropDown.classList.toggle("show");
+function dropdownHandler(e) {
+	e.stopPropagation();
+	dropDown.classList.toggle('show');
 }
 // Hide dropdown if you click on something else
 function hideMenuHandler(e) {
-	console.log('test');
-	// e.preventDefault();
-	// if(!e.target.matches('.dropdown-link')) {
-	// 	var dropdownContent = document.getElementsByClassName(".dropdown-menu");
-	// 	var i;
-	// 	for (i = 0; i < dropdownContent.length; i++) {
-	// 		var openDropdown = dropdownContent[i];
-	// 		if(openDropdown.classList.contains('show')) {
-	// 			openDropdown.classList.remove('show');
-	// 		}
-	// 	}
-	// }
+	dropDown.classList.remove('show');
 }
 
 // Sticky navbar 
-
-
 navLinks.forEach(link => link.addEventListener('click', smoothScroll));
 ctaLinks.forEach(link => link.addEventListener('click', smoothScroll));
-// dropdownLink.addEventListener('click', dropdownMenu);
-// window.addEventListener('click', hideMenuHandler);
+dropdownLink.addEventListener('click', dropdownHandler);
+window.addEventListener('click', hideMenuHandler);
