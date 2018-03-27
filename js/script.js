00c31ba5a2219c26b83e3782091a86c8c3c9b025
msunji <mae.sunji@gmail.com>
2018-03-27 21:44:18 +0800

    Fixed dropdown menu issue

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const dropdownDesktop= document.querySelectorAll('.dropdown-link');
 const dropDown = document.querySelectorAll('.dropdown-desktop');
 const carats = document.querySelectorAll('.down-carat');
 
+
+
 // Smooth Scroll
 // function smoothScroll(e) {
 // 	e.preventDefault();
@@ -11,30 +13,28 @@ const carats = document.querySelectorAll('.down-carat');
 // 	document.querySelector(element).scrollIntoView({ behavior: 'smooth' });
 // }
 
-// Dropdown Menu
-function dropdownHandlerDesktop(e) {
-	e.preventDefault();
-	e.stopPropagation();
-	var dropdownNext = this.nextElementSibling;
-	var downCarat = this.childNodes[1];
-
-	console.log(e.target);
-
-	if(e.target) {
-		dropdownNext.classList.toggle('show');
-		downCarat.classList.toggle('active');	
-
-	}
-	else {
-		dropDown.forEach(menu => menu.classList.remove('show'));
-		downCarat.classList.remove('active');
-	}
-}
-
 // Hide dropdown if you click on something else
 function hideMenuHandler(e) {
-	dropDown.forEach(menu => menu.classList.remove('show'));
-	carats.forEach(carat => carat.classList.remove('active'));
+
+	// var downCarat = this.childNodes[1];
+	console.log(e);
+	dropdownDesktop.forEach(dropdown => {
+		var dropdownNext = dropdown.nextElementSibling;
+		if(dropdownNext.classList.contains('show') && dropdownNext.id !== e) {
+			dropdownNext.classList.remove('show');
+		} 
+	});
+	// dropDown.forEach(menu => menu.classList.remove('show'));
+	// carats.forEach(carat => carat.classList.remove('active'));
+}
+
+// Dropdown Menu
+function dropdownHandlerDesktop(e) {
+	var dropdownNext = this.nextElementSibling;
+	var downCarat = this.childNodes[1];
+	e.stopPropagation();
+	dropdownNext.classList.toggle('show');
+	hideMenuHandler(dropdownNext.id);
 }
 
 // Sticky navbar
